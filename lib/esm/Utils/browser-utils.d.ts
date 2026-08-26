9b14da83dfca4433a8d2cf430ddc5ab6b5c0a8fc
Avandy <reivalavandy06@gmail.com>
2026-08-27 06:09:39 +0700

    chore: untrack compiled lib, restore official gitignore

--- lib/esm/Utils/browser-utils.d.ts
+++
@@ -1,4 +0,0 @@
-import type { BrowsersMap } from '../Types';
-export declare const Browsers: BrowsersMap;
-export declare const getPlatformId: (browser: string) => string;
-//# sourceMappingURL=browser-utils.d.ts.map
--- lib/esm/WAUSync/Protocols/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './USyncDeviceProtocol';
-export * from './USyncContactProtocol';
-export * from './USyncStatusProtocol';
-export * from './USyncDisappearingModeProtocol';
-export * from './USyncUsernameProtocol';
-//# sourceMappingURL=index.d.ts.map
